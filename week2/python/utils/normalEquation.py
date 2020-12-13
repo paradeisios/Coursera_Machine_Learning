f2a9569cfdae49bf00f3aef8d997cbbcb124b104
paradeisios <p.boulakis@student.maastrichtuniversity.nl>
2020-12-13 18:20:13 +0200

    First commit

--- week2/python/utils/normalEquation.py
+++ week2/python/utils/normalEquation.py
@@ -5,12 +5,9 @@ Created on Mon Aug 31 14:24:22 2020
 
 @author: paradeisios
 """
-import numpy as np
-
 def normalEquation(X,y):
     
-    inverse = np.linalg.inv(np.dot(X.T,X))
-    theta = np.dot(inverse.dot(X.T),y)
+    theta = (X.T @ y) / (X.T @ X)
 
     
     return theta
