3736a680c5a579c4fa9987ea4f8a0db639023c00
Paradeisios <p.boulakis@student.maastrichtuniversity.nl>
2021-05-04 23:06:54 +0300

    minor cleaning

--- week2/python/utils/computeCost.py
+++ week2/python/utils/computeCost.py
@@ -6,8 +6,8 @@ def computeCost(theta,X,y):
     m = y.shape[0] 
     J = 0
     
-    hypothesis = X.dot(theta) - y
-    J = (1.0/(2.0*m)) * np.sum(np.square(hypothesis))
+    hypothesis = X@(theta) - y
+    J = (1.0/(2.0*m)) * np.square(hypothesis).sum()
     
     return J
     
